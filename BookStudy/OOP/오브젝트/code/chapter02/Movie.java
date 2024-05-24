35c70157cbbc86b6d204e02e4b2b3cd2df371a27
최상현 <itchoi0429@gmail.com>
2024-05-24 19:21:57 +0900

    몽고 DB 앱 설계, 복제 셋

--- BookStudy/OOP/오브젝트/code/chapter02/Movie.java
+++ BookStudy/OOP/오브젝트/code/chapter02/Movie.java
@@ -1,7 +1,13 @@
 package chapter02;
 
+import javax.management.Query;
+
 public class Movie {
+
     public Money getFee() {
         return null;
+    }
+
+    public Query calculateMovieFee(Screening screening) {
     }
 }
--- BookStudy/OOP/오브젝트/code/chapter02/Reservation.java
+++ BookStudy/OOP/오브젝트/code/chapter02/Reservation.java
@@ -1,4 +1,7 @@
 package chapter02;
 
 public class Reservation {
+    public Reservation(Customer customer, Object o, int audienceCount) {
+
+    }
 }
